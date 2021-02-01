ef3fdf5557b0bd913aa5d97cda4cfbfaf74bd8cd
charlottedrb <c.derbaghdassarian@gmail.com>
2021-02-01 15:08:56 +0100

    liste de course OK

--- tp1_nodejs/server.js
+++ tp1_nodejs/server.js
@@ -1,21 +1,38 @@
 const express = require("express")
 // Créer le serveur
 const app = express()
+const bodyparser = require("body-parser")
 
+let groceryList = [
+    { name: "Bière", quantity: 42 },
+    { name: "Papier toilette", quantity: 120 },
+    { name: "Pâtes", quantity: 73 },
+]
+
+app.set("view engine", "ejs")
 // route static = transformer node js en apache
 app.use("/css", express.static(__dirname + "/css"))
+app.use(bodyparser.urlencoded({extended: false}))
 
 // req = request = ce qui est demandé 
 // res = response = ce qu'on envoie au client
 
 app.get("/", (req, res) => {
-    console.log("racine")
-    res.sendFile(__dirname + "/index.html")
+    res.render("index", { list: groceryList})
 })
 
-app.get("/my-awesome-file.PDF", (req, res) => {
-    console.log("autre route")
-    res.sendFile(__dirname + "/index.html")
+app.get("/list", (req, res) => {
+    res.json(groceryList)
+})
+
+
+app.post("/add", (req, res) => {
+    let newStuffToBuy = {
+        name: req.body.name,
+        quantity: req.body.quantity
+    }
+    groceryList.push(newStuffToBuy)
+    res.redirect("/")
 })
 
 app.listen(1337)
